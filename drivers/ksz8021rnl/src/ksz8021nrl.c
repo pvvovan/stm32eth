5bfc0af800e8a44b7cdb7844e6d8e7d904c60167
Roman Piskun <rpiskun@gmail.com>
2019-12-21 22:35:34 +0000

    Add KSZ8021 cmake file

--- drivers/ksz8021rnl/src/ksz8021nrl.c
+++ drivers/ksz8021rnl/src/ksz8021nrl.c
@@ -2,11 +2,42 @@
 #include "stm32f4xx_hal_eth.h"
 #include "ksz8021nrl.h"
 
-static void ksz8021_gpio_init();
+static ETH_HandleTypeDef h_eth;
+
+static void ksz8021_gpio_init(ETH_HandleTypeDef *heth);
 
 int ksz8021_init(uint8_t *mac_addr)
 {
+    HAL_StatusTypeDef status = HAL_ERROR;
+
     if (mac_addr == NULL)
+    {
+        return -1;
+    }
+
+    status = HAL_ETH_RegisterCallback(
+                &h_eth,
+                HAL_ETH_MSPINIT_CB_ID,
+                ksz8021_gpio_init);
+
+    if (status != HAL_OK)
+    {
+        return -1;
+    }
+
+    h_eth.Instance = ETH;
+    h_eth.Init.MACAddr = mac_addr;
+    h_eth.Init.AutoNegotiation = ETH_AUTONEGOTIATION_ENABLE;
+    h_eth.Init.Speed = ETH_SPEED_100M;
+    h_eth.Init.DuplexMode = ETH_MODE_FULLDUPLEX;
+    h_eth.Init.MediaInterface = ETH_MEDIA_INTERFACE_RMII;
+    h_eth.Init.RxMode = ETH_RXPOLLING_MODE;
+    h_eth.Init.ChecksumMode = ETH_CHECKSUM_BY_HARDWARE;
+    h_eth.Init.PhyAddress = PHY_ADDRESS;
+
+    /* configure ethernet peripheral (GPIOs, clocks, MAC, DMA) */
+    status = HAL_ETH_Init(&h_eth);
+    if (status != HAL_OK)
     {
         return -1;
     }
@@ -20,9 +51,10 @@ int ksz8021_init(uint8_t *mac_addr)
  * @brief PHY GPIO init
  *
  */
-static void ksz8021_gpio_init()
+static void ksz8021_gpio_init(ETH_HandleTypeDef *heth)
 {
     GPIO_InitTypeDef gpio;
+    (void)heth;
 
     /* Enable GPIOs clocks */
     __HAL_RCC_GPIOA_CLK_ENABLE();
--- inc/stm32f4xx_hal_conf.h
+++ inc/stm32f4xx_hal_conf.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    stm32f4xx_hal_conf_template.h
   * @author  MCD Application Team
-  * @brief   HAL configuration template file. 
+  * @brief   HAL configuration template file.
   *          This file should be copied to the application folder and renamed
   *          to stm32f4xx_hal_conf.h.
   ******************************************************************************
@@ -17,7 +17,7 @@
   *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
-  */ 
+  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F4xx_HAL_CONF_H
@@ -32,9 +32,9 @@
 
 /* ########################## Module Selection ############################## */
 /**
-  * @brief This is the list of modules to be used in the HAL driver 
+  * @brief This is the list of modules to be used in the HAL driver
   */
-#define HAL_MODULE_ENABLED  
+#define HAL_MODULE_ENABLED
 // #define HAL_ADC_MODULE_ENABLED
 // #define HAL_CAN_MODULE_ENABLED
 // #define HAL_CAN_LEGACY_MODULE_ENABLED
@@ -88,9 +88,9 @@
 /**
   * @brief Adjust the value of External High Speed oscillator (HSE) used in your application.
   *        This value is used by the RCC HAL module to compute the system frequency
-  *        (when HSE is used as system clock source, directly or through the PLL).  
+  *        (when HSE is used as system clock source, directly or through the PLL).
   */
-#if !defined  (HSE_VALUE) 
+#if !defined  (HSE_VALUE)
   #define HSE_VALUE              8000000U /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
@@ -101,7 +101,7 @@
 /**
   * @brief Internal High Speed oscillator (HSI) value.
   *        This value is used by the RCC HAL module to compute the system frequency
-  *        (when HSI is used as system clock source, directly or through the PLL). 
+  *        (when HSI is used as system clock source, directly or through the PLL).
   */
 #if !defined  (HSI_VALUE)
   #define HSI_VALUE              16000000U /*!< Value of the Internal oscillator in Hz */
@@ -110,7 +110,7 @@
 /**
   * @brief Internal Low Speed oscillator (LSI) value.
   */
-#if !defined  (LSI_VALUE) 
+#if !defined  (LSI_VALUE)
  #define LSI_VALUE               32000U    /*!< LSI Typical Value in Hz */
 #endif /* LSI_VALUE */                     /*!< Value of the Internal Low Speed oscillator in Hz
                                                 The real value may vary depending on the variations
@@ -128,8 +128,8 @@
 
 /**
   * @brief External clock source for I2S peripheral
-  *        This value is used by the I2S HAL module to compute the I2S clock source 
-  *        frequency, this source is inserted directly through I2S_CKIN pad. 
+  *        This value is used by the I2S HAL module to compute the I2S clock source
+  *        frequency, this source is inserted directly through I2S_CKIN pad.
   */
 #if !defined  (EXTERNAL_CLOCK_VALUE)
   #define EXTERNAL_CLOCK_VALUE     12288000U /*!< Value of the External oscillator in Hz*/
@@ -141,7 +141,7 @@
 /* ########################### System Configuration ######################### */
 /**
   * @brief This is the HAL system configuration section
-  */     
+  */
 #define  VDD_VALUE                    3300U /*!< Value of VDD in mv */
 #define  TICK_INT_PRIORITY            0x0FU /*!< tick interrupt priority */
 #define  USE_RTOS                     0U
@@ -158,7 +158,7 @@
 #define  USE_HAL_DFSDM_REGISTER_CALLBACKS       0U /* DFSDM register callback disabled     */
 #define  USE_HAL_DMA2D_REGISTER_CALLBACKS       0U /* DMA2D register callback disabled     */
 #define  USE_HAL_DSI_REGISTER_CALLBACKS         0U /* DSI register callback disabled       */
-#define  USE_HAL_ETH_REGISTER_CALLBACKS         0U /* ETH register callback disabled       */
+#define  USE_HAL_ETH_REGISTER_CALLBACKS         1U /* ETH register callback disabled       */
 #define  USE_HAL_HASH_REGISTER_CALLBACKS        0U /* HASH register callback disabled      */
 #define  USE_HAL_HCD_REGISTER_CALLBACKS         0U /* HCD register callback disabled       */
 #define  USE_HAL_I2C_REGISTER_CALLBACKS         0U /* I2C register callback disabled       */
@@ -190,7 +190,7 @@
 
 /* ########################## Assert Selection ############################## */
 /**
-  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the
   *        HAL drivers code
   */
 /* #define USE_FULL_ASSERT    1U */
@@ -207,7 +207,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 /**
-  * @brief Include module's header file 
+  * @brief Include module's header file
   */
 
 #ifdef HAL_RCC_MODULE_ENABLED
@@ -225,7 +225,7 @@
 #ifdef HAL_DMA_MODULE_ENABLED
   #include "stm32f4xx_hal_dma.h"
 #endif /* HAL_DMA_MODULE_ENABLED */
-   
+
 #ifdef HAL_CORTEX_MODULE_ENABLED
   #include "stm32f4xx_hal_cortex.h"
 #endif /* HAL_CORTEX_MODULE_ENABLED */
@@ -247,7 +247,7 @@
 #endif /* HAL_CRC_MODULE_ENABLED */
 
 #ifdef HAL_CRYP_MODULE_ENABLED
-  #include "stm32f4xx_hal_cryp.h" 
+  #include "stm32f4xx_hal_cryp.h"
 #endif /* HAL_CRYP_MODULE_ENABLED */
 
 #ifdef HAL_DMA2D_MODULE_ENABLED
@@ -272,7 +272,7 @@
 #ifdef HAL_FLASH_MODULE_ENABLED
   #include "stm32f4xx_hal_flash.h"
 #endif /* HAL_FLASH_MODULE_ENABLED */
- 
+
 #ifdef HAL_SRAM_MODULE_ENABLED
   #include "stm32f4xx_hal_sram.h"
 #endif /* HAL_SRAM_MODULE_ENABLED */
@@ -287,8 +287,8 @@
 
 #ifdef HAL_PCCARD_MODULE_ENABLED
   #include "stm32f4xx_hal_pccard.h"
-#endif /* HAL_PCCARD_MODULE_ENABLED */ 
-  
+#endif /* HAL_PCCARD_MODULE_ENABLED */
+
 #ifdef HAL_SDRAM_MODULE_ENABLED
   #include "stm32f4xx_hal_sdram.h"
 #endif /* HAL_SDRAM_MODULE_ENABLED */
@@ -372,7 +372,7 @@
 #ifdef HAL_HCD_MODULE_ENABLED
  #include "stm32f4xx_hal_hcd.h"
 #endif /* HAL_HCD_MODULE_ENABLED */
-   
+
 #ifdef HAL_DSI_MODULE_ENABLED
  #include "stm32f4xx_hal_dsi.h"
 #endif /* HAL_DSI_MODULE_ENABLED */
@@ -411,7 +411,7 @@
   * @brief  The assert_param macro is used for function's parameters check.
   * @param  expr If expr is false, it calls assert_failed function
   *         which reports the name of the source file and the source
-  *         line number of the call that failed. 
+  *         line number of the call that failed.
   *         If expr is true, it returns no value.
   * @retval None
   */
@@ -428,6 +428,6 @@
 #endif
 
 #endif /* __STM32F4xx_HAL_CONF_H */
- 
+
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

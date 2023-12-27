c7a2c6cdc7f65e21ba3fc17e4def96a4f6e890ff
Volodymyr Petrovskyy <pvvovan@gmail.com>
2023-12-27 09:35:49 +0100

    Enable full access to RAM_D2

--- Src/main.c
+++ Src/main.c
@@ -273,7 +273,7 @@ static void MPU_Config(void)
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.BaseAddress = 0x30000000uL;
   MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
-  MPU_InitStruct.AccessPermission = MPU_REGION_PRIV_RW;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
   MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
   MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
